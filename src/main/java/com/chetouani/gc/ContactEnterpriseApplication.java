466223b5ff856090d7b881c695fcde3338707e28
Chetouani Abdelhalim <chetouaniabdelhalim@gmail.com>
2023-12-14 15:01:51 +0100

    Create new project with maven and needed dependencies

---
+++ src/main/java/com/chetouani/gc/ContactEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.chetouani.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactEnterpriseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chetouani/gc/ContactEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chetouani.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactEnterpriseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
