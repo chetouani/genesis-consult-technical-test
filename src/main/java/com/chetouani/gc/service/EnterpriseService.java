4efed287b41310722a8f371c48f26f63557056fc
Chetouani Abdelhalim <chetouaniabdelhalim@gmail.com>
2023-12-20 10:31:25 +0100

    fix missing import

--- src/main/java/com/chetouani/gc/service/EnterpriseService.java
+++ src/main/java/com/chetouani/gc/service/EnterpriseService.java
@@ -8,7 +8,8 @@ import com.chetouani.gc.repository.EnterpriseRepositoryInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
- 
+
+import java.util.List;
 import java.util.Set;
 
 @Service
